9df863538766a39bf2f7960a3a09d6affff56295
Eloi Dieme <eloidieme@gmail.com>
2024-02-23 22:18:07 +0100

    implementation of solver done

--- src/stable_fluids.py
+++ src/stable_fluids.py
@@ -40,9 +40,6 @@ def forcing_function(time, point):
 
     return forced_value
 
-def advect():
-    pass
-
 def main():
     element_length = DOMAIN_SIZE / (N_POINTS - 1)
     scalar_shape = (N_POINTS, N_POINTS)
@@ -68,6 +65,116 @@ def main():
         signature="(),(d)->(d)",
     )
 
+    def partial_derivative_x(field):
+        diff = np.zeros_like(field)
+
+        diff[1:-1, 1:-1] = (
+            (
+                field[2: , 1:-1]
+                -
+                field[0:-2, 1:1]
+            ) / (
+                2 * element_length
+            )
+        )
+
+    def partial_derivative_y(field):
+        diff = np.zeros_like(field)
+
+        diff[1:-1, 1:-1] = (
+            (
+                field[1:-1 , 2:]
+                -
+                field[1:-1, 0:-2]
+            ) / (
+                2 * element_length
+            )
+        )
+
+    def laplace(field):
+        diff = np.zeros_like(field)
+
+        diff[1:-1, 1:-1] = (
+            (
+                field[0:-2, 1:-1]
+                +
+                field[1:-1, 0:-2]
+                - 4 *
+                field[1:-1, 1:-1]
+                +
+                field[2:  , 1:-1]
+                +
+                field[1:-1, 2:  ]
+            ) / (
+                element_length**2
+            )
+        )
+
+        return diff
+    
+    def divergence(vector_field):
+        divergence_applied = (
+            partial_derivative_x(vector_field[..., 0])
+            +
+            partial_derivative_y(vector_field[..., 1])
+        )
+
+        return divergence_applied
+    
+    def gradient(field):
+        gradient_applied = np.concatenate(
+            (
+                partial_derivative_x(field)[..., np.newaxis],
+                partial_derivative_y(field)[..., np.newaxis],
+            ),
+            axis=-1
+        )
+
+        return gradient_applied
+
+    def advect(field, vector_field):
+        backtraced_positions = np.clip(
+            (
+                coordinates
+                -
+                TIME_STEP_LENGTH
+                *
+                vector_field
+            ),
+            0.0,
+            DOMAIN_SIZE,
+        )
+
+        advected_field = interpolate.interpn(
+            points=(x, y),
+            values=field,
+            xi=backtraced_positions,
+        )
+
+        return advected_field
+    
+    def diffusion_operator(vector_field_flattened):
+        vector_field = vector_field_flattened.reshape(vector_shape)
+
+        diffusion_applied = (
+            vector_field
+            *
+            KINEMATIC_VISCOSITY
+            /
+            TIME_STEP_LENGTH
+            *
+            laplace(vector_field)
+        )
+
+        return diffusion_applied.flatten()
+    
+    def poisson_operator(field_flattened):
+        field = field_flattened.reshape(scalar_shape)
+
+        poisson_applied = laplace(field)
+
+        return poisson_applied
+
     velocities_prev = np.zeros(vector_shape)
 
     time_current = 0.0
@@ -89,6 +196,42 @@ def main():
         )
 
         # 2 - Nonlinear convection
+        velocities_advected = advect(
+            field=velocities_forces_applied,
+            vector_field=velocities_forces_applied
+        )
+
+        # 3 - Diffuse
+        velocities_diffused = splinalg.cg(
+            A = splinalg.LinearOperator(
+                shape=(vector_dof, vector_dof),
+                matvec=diffusion_operator,
+            ),
+            b = velocities_advected.flatten(),
+            maxiter=MAX_ITER_CG,
+        )[0].reshape(vector_shape)
+
+        # 4.1 - Compute pressure correction
+        pressure = splinalg.cg(
+            A = splinalg.LinearOperator(
+                shape=(scalar_dof, scalar_dof),
+                matvec=poisson_operator,
+            ),
+            b = divergence(velocities_diffused).flatten(),
+            maxiter=MAX_ITER_CG
+        )[0].reshape(scalar_shape)
+
+        # 4.2 - Correct the velocities to be incompressible
+        velocities_projected = (
+            velocities_diffused
+            -
+            gradient(pressure)
+        )
+
+        # next time step
+        velocities_prev = velocities_projected
+
+
 
 
     print(coordinates.shape)
